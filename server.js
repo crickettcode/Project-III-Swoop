eec60b5361705007cd61213458ceb490105033a5
crickett <crickettcode@gmail.com>
2018-04-30 13:04:02 -0400

    added concurrently

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
     res.send('What it do little Moon')
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(" What it do Pikachu " + PORT);
 })
