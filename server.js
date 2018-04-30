9d95221964df3b21bc3cf920d804b43af5c897e3
crickett <crickettcode@gmail.com>
2018-04-30 13:28:41 -0400

    initial heroku commit

--- server.js
+++ server.js
@@ -21,6 +21,11 @@ app.get('/', (req, res) => {
     res.send('What it do little Moon')
 })
 
+app.use(express.static(__dirname + '/client/build/'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+})
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(" What it do Pikachu " + PORT);
