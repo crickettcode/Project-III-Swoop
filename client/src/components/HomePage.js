1c639e5d1cfc3f14a5546099f4eaa30b3930f15f
crickett <crickettcode@gmail.com>
2018-05-03 19:09:19 -0400

    homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import styled from 'styled-components'
+
 
 class HomePage extends Component {
 
     render() {
         return (
 
-            <div>
+            <Homestyle>
 
-                HomePage
+                <h1> Swoop</h1>
 
-                <div>
-                    <Link to='/users'>Swoop</Link>
-                </div>
+                <footer>
+                    <Link to='/users'>Try Me</Link>
+                </footer>
 
-            </div>)
+            </Homestyle>)
     }
 
 }
 
 
 
-export default HomePage 
+export default HomePage
+
+
+const Homestyle = styled.div`
+
+    flex-direction: column;
+
+
+
+h1{
+
+}
+section {
+  flex-grow: 1;
+}
+
+@media (max-width: 400px){
+  main {
+    flex-direction: column;
+  }
+
+
+
+
+`
+
+
+
+
