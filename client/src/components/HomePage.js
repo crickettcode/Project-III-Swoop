07d28ee10b0a9e0eca43b51e36fb16c1ff6f5e33
crickett <crickettcode@gmail.com>
2018-05-03 19:21:13 -0400

    swoop

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -12,7 +12,7 @@ class HomePage extends Component {
             <Homestyle>
 
                 <h1> Swoop</h1>
-
+                <p>The app that makes the Plan, so that you can be Spontaneous</p>
                 <footer>
                     <Link to='/users'>Try Me</Link>
                 </footer>
@@ -30,10 +30,28 @@ export default HomePage
 const Homestyle = styled.div`
 
     flex-direction: column;
+background:#FFF1D2;
+padding:45px;
 
+p {
+    font-family:"cursive";
 
+}
+
+footer{
+    background: #4C979B;
+    color:whitesmoke;
+    border-radius:20px;
+}
+
+a {
+    color:white;
+}
 
 h1{
+    display:flex;
+    justify-content:center;
+    font-family:"cursive";
 
 }
 section {
